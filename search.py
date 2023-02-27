e6508be79f6259fdd767a2f8aaf0d98610dfc7ed
Ali Zahid Raja <alizahidrajaa@gmail.com>
2023-02-27 16:38:35 +0500

    Resolved errors

--- search.py
+++ search.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import cohere
-import numpy as np
-import faiss
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
