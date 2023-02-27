4286fda7041f85c56d64573f5f4d9bb3c42fa780
Ali Zahid Raja <alizahidrajaa@gmail.com>
2023-02-27 21:54:15 +0500

    removed analytics

--- search.py
+++ search.py
@@ -3,20 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import streamlit.components.v1 as components
+import streamlit_analytics
 
-components.html(
-    """
-      <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N39RFTGS8Q"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-N39RFTGS8Q');
-        </script>
-    """,
-    width=0, height=0
-)
 class AyatSearch():
 
     def __init__(self, path):
@@ -50,52 +38,52 @@ class AyatSearch():
         return top_paragraphs
 
 
+with streamlit_analytics.track():
+    st.title("Welcome to Islam & AI")
+    st.write("Your personal AI assistant that uses Quranic Ayats to search for your queries! Our model is based on Natural Language Processing techniques and is designed to help you find relevant information from the Quran quickly and easily. Whether you have a question about Islamic beliefs, practices, or anything else related to Islam, just ask our AI assistant and it will provide you with the most relevant Quranic Ayats to answer your query.")
+    st.write("This is the initial model for a very big project, please give feedback, share & let us know about any questions you might have")
 
-st.title("Welcome to Islam & AI")
-st.write("Your personal AI assistant that uses Quranic Ayats to search for your queries! Our model is based on Natural Language Processing techniques and is designed to help you find relevant information from the Quran quickly and easily. Whether you have a question about Islamic beliefs, practices, or anything else related to Islam, just ask our AI assistant and it will provide you with the most relevant Quranic Ayats to answer your query.")
-st.write("This is the initial model for a very big project, please give feedback, share & let us know about any questions you might have")
+    search = AyatSearch("data/main_df.csv")
 
-search = AyatSearch("data/main_df.csv")
-
-st.subheader("Enter your query:")
-query = st.text_input("", "Importance of Prayer")#st.session_state.query)
-# st.session_state.query = query
-st.subheader("Select the number of queries:")
-x = st.slider("", 2, 25, 3)
-# from translate import Translator
+    st.subheader("Enter your query:")
+    query = st.text_input("", "Importance of Prayer")#st.session_state.query)
+    # st.session_state.query = query
+    st.subheader("Select the number of queries:")
+    x = st.slider("", 2, 25, 3)
+    # from translate import Translator
 
 
-# print 
+    # print 
 
-# translator = Translator(to_lang=option.lower())
-# translation = translator.translate(query  )
-results = search.query(query, int(x))
+    # translator = Translator(to_lang=option.lower())
+    # translation = translator.translate(query  )
+    results = search.query(query, int(x))
 
-st.title("**Results:**")
+    st.title("**Results:**")
 
-for r in results:
-    # Questions
-    text = r.split("|")
-    st.subheader(f"{text[1]}")
-    st.write(f"**- Surah Name: {text[5]} | {text[4]} | {text[6]} | {text[0]}**")
-    st.write(f"**- Surah No. {text[2]} | Ayat No. {text[3]}**")
-    st.write(f"**- Surah Revealed in {text[7]}**")
+    for r in results:
+        # Questions
+        text = r.split("|")
+        st.subheader(f"{text[1]}")
+        st.write(f"**- Surah Name: {text[5]} | {text[4]} | {text[6]} | {text[0]}**")
+        st.write(f"**- Surah No. {text[2]} | Ayat No. {text[3]}**")
+        st.write(f"**- Surah Revealed in {text[7]}**")
 
-    # Answers
-    st.subheader("Translations:")
-    translations = text[-2].split(";")
-    for i in range(len(translations)):
-        if len(translations[i])>2:
-            st.write(f"{i+1}: {translations[i]}")
+        # Answers
+        st.subheader("Translations:")
+        translations = text[-2].split(";")
+        for i in range(len(translations)):
+            if len(translations[i])>2:
+                st.write(f"{i+1}: {translations[i]}")
 
-    st.subheader("Tafaseer:")
-    tafaseer = text[-1].split(";")
-    for i in range(len(tafaseer)):
-        if len(tafaseer[i])>2:
-            st.write(f"{i+1}: {tafaseer[i]}")
-        
-    st.subheader("-"*70)
-# st.write(f"{results}")
+        st.subheader("Tafaseer:")
+        tafaseer = text[-1].split(";")
+        for i in range(len(tafaseer)):
+            if len(tafaseer[i])>2:
+                st.write(f"{i+1}: {tafaseer[i]}")
+            
+        st.subheader("-"*70)
+    # st.write(f"{results}")
 
 
 
